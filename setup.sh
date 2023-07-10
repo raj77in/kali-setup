a506f4919dde6a8fe901ee677b6f38743c5766a6
Amit Agarwal <gmail@amit-agarwal.co.in>
2023-07-10 16:39:51 +0530

    Some more code changes.

--- setup.sh
+++ setup.sh
@@ -21,6 +21,7 @@
 
 script_path=$(cd $(dirname $0); pwd)
 param=${1:-none}
+TOOLS="$HOME/tools"
 
 function dfonts()
 {
@@ -61,18 +62,62 @@ function create_link()
     ln -s $script_path/$1 $dest
 }
 
+function config_JSParser() {
+  cd $TOOLS/JSParser || return
+  python setup.py install --user
+}
+
+function config_Sublist3r()
+{
+  cd $TOOLS/Sublist3r || return
+  pip install -r requirements.txt --user
+}
+
+function config_wpscan()
+{
+  cd $TOOLS/wpscan || return
+  sudo gem install bundler && bundle install --without test
+}
+
+function config_massdns()
+{
+  cd $TOOLS/massdns || return
+  make
+}
+
+function config_asnlookup()
+{
+  cd $TOOLS/asnlookup || return
+  pip install -r requirements.txt --user
+}
+
+function config_SecLists()
+{
+  cd "$TOOLS/SecLists/Discovery/DNS/" || return
+  ##THIS FILE BREAKS MASSDNS AND NEEDS TO BE CLEANED
+  sudo ln -s $TOOLS/Seclists /usr/share/seclists
+}
+
 function gitdw()
 {
-    cd ~/tools
-    gitd=$(basename $1)
+    cd $TOOLS || return
+    echo "Check out $1"
+    gitd="$(echo $(basename "$1") |sed 's/.git//')"
     if [[ -d $gitd ]]
     then
-        { cd $gitd;
+        { cd "$gitd" || return
             git pull
         }
     else
-        git clone https://github.com/$1
+        git clone "https://github.com/$1"
     fi
+    if [[ $(type -t "config_${gitd}" ) == "function" ]]
+    then
+      "config_${gitd}"
+    else
+      echo "There is no function called config_$gitd to configure $1"
+    fi
+    echo "Done"
 }
 
 
@@ -82,8 +127,8 @@ then
     echo "Check if bashrc sources ~/.bash_aliases, if not source the same"
     [[ ! -d ~/.config/rofi ]] && mkdir ~/.config/rofi
 
-    [[ ! -d $script_path/my ]] && mkdir -p $script_path/my/bash
-    echo 'echo "customize here :: $0"'> $script_path/my/bash/00-default.sh
+    [[ ! -d $script_path/my ]] && mkdir -p "$script_path/my/bash"
+    echo 'echo "customize here :: $0"'> "$script_path/my/bash/00-default.sh"
 
 
     # ln -s kali-setup/config/.bashrc
@@ -116,7 +161,7 @@ then
 
     ## Fonts
     [[ ! -d ~/.fonts ]] && mkdir ~/.fonts
-    cd ~/.fonts
+    cd ~/.fonts || return
     download_fonts
     # Once the fonts are downlaoded to the fonts folder, refresh cache'
     fc-cache
@@ -153,7 +198,7 @@ then
     ## Theme
 
     [[ ! -d /.themes ]] && mkdir ~/.themes
-    cd ~/.themes
+    cd ~/.themes || return
     wget --header 'Host: ocs-dl.fra1.cdn.digitaloceanspaces.com' --user-agent 'Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --referer 'https://www.xfce-look.org/' --header 'Upgrade-Insecure-Requests: 1' --header 'Sec-Fetch-Dest: document' --header 'Sec-Fetch-Mode: navigate' --header 'Sec-Fetch-Site: cross-site' --header 'Sec-Fetch-User: ?1' 'https://ocs-dl.fra1.cdn.digitaloceanspaces.com/data/files/1515879642/XFCE-D-PRO-1.6.tar.xz?response-content-disposition=attachment%3B%2520XFCE-D-PRO-1.6.tar.xz&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=RWJAQUNCHT7V2NCLZ2AL%2F20221029%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20221029T132254Z&X-Amz-SignedHeaders=host&X-Amz-Expires=60&X-Amz-Signature=4d767b84ab48d16f476f8bb8af31924e9cd2fc86e712d2f2fd896c44d170903c' --output-document 'XFCE-D-PRO-1.6.tar.xz'
     tar xvf XFCE-D-PRO-1.6.tar.xz
     rm -rf XFCE-D-PRO-1.6.tar.xz
@@ -165,7 +210,7 @@ then
     ## Some nerd fonts
 
     [[ ! -d ~/.fonts ]] && mkdir ~/.fonts
-    cd ~/.fonts
+    cd ~/.fonts || return
     wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/CascadiaCode.zip
     unzip CascadiaCode.zip && rm CascadiaCode.zip
     wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Go-Mono.zip
@@ -187,131 +232,57 @@ sudo apt install -y $(grep -v '^#' pkgs|tr '\n' ' ')
 # apt autoremove -y
 sudo apt autoclean -y
 
+echo "Remove some tools from apt, its old"
+# sudo apt remove --purge seclists python3-impacket
+
+## Downlaod git repos to be used
+
+for i in nahamsec/JSParser.git \
+	aboul3la/Sublist3r.git \
+	tomdev/teh_s3_bucketeers.git \
+	wpscanteam/wpscan.git \
+	maurosoria/dirsearch.git \
+	nahamsec/lazys3.git \
+	jobertabma/virtual-host-discovery.git \
+	sqlmapproject/sqlmap.git \
+	nahamsec/lazyrecon.git \
+	blechschmidt/massdns.git \
+	yassineaboukir/asnlookup.git \
+	nahamsec/crtndstry.git \
+	danielmiessler/SecLists.git \
+	swisskyrepo/PayloadsAllTheThings \
+	Flangvik/SharpCollection \
+	jpillora/chisel \
+	carlospolop/PEASS-ng \
+	WithSecureLabs/chainsaw \
+	BloodHoundAD/BloodHound \
+	fortra/impacket.git \
+	mpgn/CrackMapExec.git \
+	ly4k/Certipy.git \
+	sebastiencs/icons-in-terminal
+do
+  gitdw "$i"
+done
+
+
 # Some python tools
-pipx install --user pwntools
+pip install --user pwntools
 
 echo "Installing aquatone"
-go get github.com/michenriksen/aquatone
+go install github.com/michenriksen/aquatone@latest
 
-echo "Installing jsparser"
-gitdw nahamsec/JSParser.git
-cd JSParser*
-sudo python setup.py install
-
-echo "installing Sublist3r"
-gitdw aboul3la/Sublist3r.git
-cd Sublist3r*
-pipx install -r requirements.txt
-
-echo "installing teh_s3_bucketeers"
-gitdw tomdev/teh_s3_bucketeers.git
-cd ~/tools/
-echo "done"
-
-
-echo "installing wpscan"
-gitdw wpscanteam/wpscan.git
-cd wpscan*
-sudo gem install bundler && bundle install --without test
-
-echo "installing dirsearch"
-gitdw maurosoria/dirsearch.git
-
-
-echo "installing lazys3"
-gitdw nahamsec/lazys3.git
-
-echo "installing virtual host discovery"
-gitdw jobertabma/virtual-host-discovery.git
-
-
-echo "installing sqlmap"
-gitdw sqlmapproject/sqlmap.git
-
-echo "installing knock.py"
-git clone https://github.com/guelfoweb/knock.git
-
-echo "installing lazyrecon"
-gitdw nahamsec/lazyrecon.git
-
-echo "installing massdns"
-gitdw blechschmidt/massdns.git
-cd massdns
-make
-
-echo "installing asnlookup"
-gitdw yassineaboukir/asnlookup.git
-
-cd asnlookup
-pipx install -r requirements.txt
-
-echo "installing httprobe"
-go get -u github.com/tomnomnom/httprobe
-
-echo "installing unfurl"
-go get -u github.com/tomnomnom/unfurl
-
-echo "installing waybackurls"
-go get github.com/tomnomnom/waybackurls
-
-echo "installing crtndstry"
-gitdw nahamsec/crtndstry.git
-
-echo "downloading Seclists"
-cd ~/tools/
-gitdw danielmiessler/SecLists.git
-cd ~/tools/SecLists/Discovery/DNS/
-##THIS FILE BREAKS MASSDNS AND NEEDS TO BE CLEANED
-cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
-
-echo "Remove some tools from apt, its old"
-sudo apt remove --purge seclists python-impacket
-
-ln -s ~/tools/Seclists /usr/share/seclists
-
-echo "Cloning PayloadAllTheThings"
-gitdw swisskyrepo/PayloadsAllTheThings
-
-
-echo "Sharp collections"
-gitdw Flangvik/SharpCollection
-
-echo "Chisel for tunneling"
-gitdw jpillora/chisel
-
-echo "PEASS-ng"
-gitdw carlospolop/PEASS-ng
-
-echo "Chainsaw"
-gitdw WithSecureLabs/chainsaw
-
-echo "Bloodhound"
-gitdw BloodHoundAD/BloodHound
-
-
-echo "impacket latest"
-gitdw fortra/impacket.git
-
-echo "CrackMapExec"
-gitdw mpgn/CrackMapExec.git
-
-echo certipy
-gitdw ly4k/Certipy.git
-
-echo "Downloading icons-in-terminal"
-gitdw sebastiencs/icons-in-terminal 
 
 echo "Downloading binaries"
+[[ ! -d $TOOLS/bin ]] && mkdir $TOOLS/bin
+python ${script_path}/githubdownload.py jpillora/chisel _linux_amd64.gz $TOOLS/bin
+python ${script_path}/githubdownload.py jpillora/chisel _windows_amd64.gz $TOOLS/bin
+python ${script_path}/githubdownload.py carlospolop/PEASS-ng linpeas.sh $TOOLS/bin
+python ${script_path}/githubdownload.py carlospolop/PEASS-ng winPEASx64.exe $TOOLS/bin
+python ${script_path}/githubdownload.py WithSecureLabs/chainsaw chainsaw_all_ $TOOLS/bin
+python ${script_path}/githubdownload.py BloodHoundAD/BloodHound BloodHound-linux-x64.zip $TOOLS/bin
 
-python githubdownload.py jpillora/chisel _linux_amd64.gz ~/tools/bin
-python githubdownload.py jpillora/chisel _windows_amd64.gz ~/tools/bin
-python githubdownload.py carlospolop/PEASS-ng linpeas.sh ~/tools/bin
-python githubdownload.py carlospolop/PEASS-ng winPEASx64.exe ~/tools/bin
-python githubdownload.py WithSecureLabs/chainsaw chainsaw_all_ ~/tools/bin
-python githubdownload.py BloodHoundAD/BloodHound BloodHound-linux-x64.zip ~/tools/bin
 
-
-echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in ~/tools"
+echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in $TOOLS"
 ls -la
 echo "One last time: don't forget to set up AWS credentials in ~/.aws/!"
 
