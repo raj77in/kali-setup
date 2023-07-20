aab78beef6fef410dbf0b2f3c7a31e8fbabbb192
Amit Agarwal <gmail@amit-agarwal.co.in>
2023-07-20 15:17:14 +0530

    Add script to check ports for common items.

---
+++ bin/check-port.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+ip=$1
+
+
+#TCP Ports
+ports=$(cat nmap/full-$ip.gnmap |grep Ports  |sed -e 's/.*Ports://' -e 's/Ignored State:.*//' -e 's/ //g' -e 's#/.[^,]*##g' | tr ',' '\n')
+for i in ${ports[*]}
+do
+  echo ;echo; echo;
+  echo "Checking port number $i on $ip"
+  echo "====================================="
+
+  echo; echo "OPTIONS http request"
+  cat <<EOF | timeout 5 nc -v $ip $i
+OPTIONS / HTTP/1.0
+
+EOF
+
+  echo; echo "GET http request"
+  cat <<EOF | timeout 5 nc -v $ip $i
+GET / HTTP/1.0
+
+EOF
+  echo; echo "POST http request"
+  cat <<EOF | timeout 5 nc -v $ip $i
+POST / HTTP/1.0
+
+EOF
+  echo; echo "help command"
+  cat <<EOF | timeout 5 nc -v $ip $i
+help
+
+EOF
+  echo; echo "? command"
+  cat <<EOF | timeout 5 nc -v $ip $i
+?
+
+EOF
+done
+
+
+## UDP Ports
+# ports=$(cat nmap/full-$ip.gnmap |grep Ports  |sed -e 's/.*Ports://' -e 's/Ignored State:.*//' -e 's/ //g' -e 's#/.[^,]*##g')
--- bin/nmap-scan.sh
+++ bin/nmap-scan.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #
-i=$1
+ip=$1
 
 nmap -p- --min-rate=5000 $ip -oA nmap/full-$ip
-ports=$(cat nmap/full-$ip.gnmap |grep Ports | |sed -e 's/.*Ports://' -e 's/Ignored State:.*//' -e 's/ //g' -e 's#/.[^,]*##g')
+ports=$(cat nmap/full-$ip.gnmap |grep Ports  |sed -e 's/.*Ports://' -e 's/Ignored State:.*//' -e 's/ //g' -e 's#/.[^,]*##g')
 
 nmap -sC -sV -A -p$ports $ip -oA nmap/ports-$ip
 
-nmap -p- --min-rate=5000 -sU $ip -oA nmap/full-udp-$ip
-ports=$(cat nmap/full-$ip.gnmap |grep Ports | |sed -e 's/.*Ports://' -e 's/Ignored State:.*//' -e 's/ //g' -e 's#/.[^,]*##g')
+sudo nmap -p- --min-rate=5000 -sU $ip -oA nmap/full-udp-$ip
+ports=$(cat nmap/full-$ip.gnmap |grep Ports  |sed -e 's/.*Ports://' -e 's/Ignored State:.*//' -e 's/ //g' -e 's#/.[^,]*##g')
 
-nmap -sU -sC -sV -A -p$ports $ip -oA nmap/ports-udp-$ip
+sudo nmap -sU -sC -sV -A -p$ports $ip -oA nmap/ports-udp-$ip
 
+sudo chown $USER:$USER nmap/*
